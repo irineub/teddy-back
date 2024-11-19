d8f8c47557f4b52dbfd247cad60eb10b823b900b
irineub <irineugmestre@gmail.com>
2024-11-19 19:21:36 -0400

    Fixes

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: `${process.env.FRONTEND_URL}`,
+    origin: `*`,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
