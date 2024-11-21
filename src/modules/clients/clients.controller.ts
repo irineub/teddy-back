864efe01ec7d7cd9eda8e598fe75c0488f40da6a
irineub <irineugmestre@gmail.com>
2024-11-20 23:13:39 -0400

    Documentação Swagger Detalhada

--- src/modules/clients/clients.controller.ts
+++ src/modules/clients/clients.controller.ts
@@ -3,7 +3,8 @@ import { ClientsService } from './clients.service';
 import { CreateClientDto } from './dto/CreateClientDto';
 import { UpdateClientDto } from './dto/UpdateClientDto';
 import { Client } from 'src/database/entities/client.entity';
-import { ApiQuery } from '@nestjs/swagger';
+import { ApiQuery, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+
 @Controller('clients')
 export class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
@@ -11,6 +12,8 @@ export class ClientsController {
   @Get()
   @ApiQuery({ name: 'page', required: false }) 
   @ApiQuery({ name: 'pageSize', required: false })
+  @ApiOperation({ summary: 'Obter todos os clientes' })
+  @ApiResponse({ status: 200, description: 'Lista de clientes', type: [Client] })
   async findAll(
     @Query('page') page: number = 1,
     @Query('pageSize') pageSize: number = 10
@@ -19,16 +22,42 @@ export class ClientsController {
   }
 
   @Post()
+  @ApiOperation({ summary: 'Criar um novo cliente' })
+  @ApiBody({
+    description: 'Detalhes do cliente para criar um novo cliente',
+    type: CreateClientDto,
+    examples: {
+      exemplo1: {
+        value: {
+          name: 'Irineu',
+          salary: 10000,
+          companyValue: 1000000,
+        },
+      },
+    },
+  })
+  @ApiResponse({ status: 201, description: 'Cliente criado com sucesso', type: Client })
+  @ApiResponse({ status: 400, description: 'Requisição inválida' })
   async create(@Body() createClientDto: CreateClientDto): Promise<Client> {
     return this.clientsService.create(createClientDto);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Obter um cliente pelo ID' })
+  @ApiResponse({ status: 200, description: 'Cliente encontrado', type: Client })
+  @ApiResponse({ status: 404, description: 'Cliente não encontrado' })
   async findOne(@Param('id') id: number): Promise<Client> {
     return this.clientsService.findOne(id);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Atualizar um cliente pelo ID' })
+  @ApiBody({
+    description: 'Detalhes do cliente para atualizar',
+    type: UpdateClientDto,
+  })
+  @ApiResponse({ status: 200, description: 'Cliente atualizado com sucesso', type: Client })
+  @ApiResponse({ status: 404, description: 'Cliente não encontrado' })
   async update(
     @Param('id') id: number,
     @Body() updateClientDto: UpdateClientDto,
@@ -37,6 +66,9 @@ export class ClientsController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Deletar um cliente pelo ID' })
+  @ApiResponse({ status: 200, description: 'Cliente deletado com sucesso' })
+  @ApiResponse({ status: 404, description: 'Cliente não encontrado' })
   async delete(@Param('id') id: number): Promise<void> {
     return this.clientsService.delete(id);
   }
